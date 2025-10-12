f0ae284089b902929f627f338fb148e5050f0a56
kz <dev@auto-s.pl>
2025-10-12 21:31:21 +0200

    first commit

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body class="text-center" style="margin-top: 100px; font-family: sans-serif;">
+    <h1>Hello World 👋</h1>
+    <p>Aplikacja dla korepetytorów działa!</p>
+</body>
+</html>
