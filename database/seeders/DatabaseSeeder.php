0ec9d8033042ea612279c4b3e2846ed3f3b73a54
kz <pilkarz4a@gmail.com>
2025-12-01 20:57:07 +0100

    tez zmiana usera do commitow na poprawnego, strike na gicie musi byc hehe

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        $this->call([
-            StudentSeeder::class
-        ]);
+        // $this->call([
+        //     StudentSeeder::class
+        // ]);
     }
 }
